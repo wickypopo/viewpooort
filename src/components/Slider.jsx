08d493e3bf972d7e4b7f174c4521739f62baed48
wickypopo <felixm@meise.at>
2026-05-08 21:45:06 +0200

    first commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -48,28 +48,27 @@ const initialSlides = [
 ];
 
 export default function Slider() {
-  const x = useMotionValue(0);
+  const slideWidth = 600;
+  const gap = 8;
+  const slideDistance = slideWidth + gap;
+  const threshold = 120;
+
+  const x = useMotionValue(-slideDistance);
   const distortion = useMotionValue(0);
+  const displacementRef = useRef(null);
 
   const smoothDistortion = useSpring(distortion, {
     stiffness: 120,
     damping: 2,
   });
+
   useMotionValueEvent(smoothDistortion, "change", (latest) => {
     displacementRef.current?.setAttribute("scale", latest);
   });
-  const displacementRef = useRef(null);
 
   const [slides, setSlides] = useState(initialSlides);
-  const [isDragging, setIsDragging] = useState(false);
 
-  let initial = 0;
-  let velocity = Math.min(Math.abs(initial) / 80, 30);
-
-  const slideWidth = 600;
-  const gap = 8;
-  const slideDistance = slideWidth + gap;
-  const threshold = 120;
+  const displaySlides = [slides[slides.length - 1], ...slides, slides[0]];
 
   function moveFirstToEnd() {
     setSlides((prev) => {
@@ -89,36 +88,34 @@ export default function Slider() {
 
   function handleDragEnd() {
     const currentX = x.get();
-    // Nach links gezogen
-    if (currentX < -threshold) {
-      animate(x, -slideDistance, {
+
+    if (currentX < -slideDistance - threshold) {
+      animate(x, -slideDistance * 2, {
         type: "spring",
         stiffness: 700,
         damping: 50,
       }).then(() => {
         moveFirstToEnd();
-        x.set(0);
+        x.set(-slideDistance);
       });
 
       return;
     }
 
-    // Nach rechts gezogen
-    if (currentX > threshold) {
-      animate(x, slideDistance, {
+    if (currentX > -slideDistance + threshold) {
+      animate(x, 0, {
         type: "spring",
         stiffness: 700,
         damping: 50,
       }).then(() => {
         moveLastToStart();
-        x.set(0);
+        x.set(-slideDistance);
       });
 
       return;
     }
 
-    // Nicht weit genug gezogen
-    animate(x, 0, {
+    animate(x, -slideDistance, {
       type: "spring",
       stiffness: 700,
       damping: 50,
@@ -126,39 +123,37 @@ export default function Slider() {
   }
 
   return (
-    <div className="overflow-hidden h-180 cursor-grab">
-      <>
-        <svg className="absolute w-0 h-0">
-          <defs>
-            <filter id="wave" x="-20%" y="-20%" width="140%" height="140%">
-              <feTurbulence
-                type="fractalNoise"
-                baseFrequency="0.004 0.012"
-                numOctaves="2"
-                result="noise"
-              />
+    <div className="h-180 cursor-grab ">
+      <svg className="absolute w-0 h-0">
+        <defs>
+          <filter id="wave" x="-20%" y="-20%" width="140%" height="140%">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.004 0.012"
+              numOctaves="2"
+              result="noise"
+            />
 
-              <feGaussianBlur in="noise" stdDeviation="6" result="softNoise" />
+            <feGaussianBlur in="noise" stdDeviation="6" result="softNoise" />
 
-              <feDisplacementMap
-                ref={displacementRef}
-                in="SourceGraphic"
-                in2="softNoise"
-                scale="0"
-                xChannelSelector="R"
-                yChannelSelector="G"
-              />
-            </filter>
-          </defs>
-        </svg>
-      </>
+            <feDisplacementMap
+              ref={displacementRef}
+              in="SourceGraphic"
+              in2="softNoise"
+              scale="0"
+              xChannelSelector="R"
+              yChannelSelector="G"
+            />
+          </filter>
+        </defs>
+      </svg>
+
       <motion.div
         drag="x"
         style={{ x }}
         onDrag={() => {
           const speed = Math.abs(x.getVelocity());
           const scale = Math.min(speed / 100, 24);
-
           distortion.set(scale);
         }}
         onDragEnd={() => {
@@ -167,21 +162,26 @@ export default function Slider() {
         }}
         className="flex gap-2 h-full"
       >
-        {slides.map((slide) => (
-          <div key={slide.id} className="min-w-[600px] h-full relative">
+        {displaySlides.map((slide, index) => (
+          <div
+            key={`${slide.id}-${index}`}
+            className="min-w-[600px] h-full relative"
+          >
             <img
               style={{ filter: "url(#wave)" }}
               src={slide.src}
               alt={slide.alt}
               draggable="false"
-              className="w-full h-4/5 bg-white object-cover"
+              className="w-full h-4/5 bg-white object-cover shadow-xl"
             />
+
             <div className="pt-4 flex flex-col items-start gap-2">
-              {" "}
               <p className="text-slate-50 text-4xl font-regular">
                 {slide.title}
               </p>
-              <p className="text-slate-200  font-light">{slide.description}</p>
+
+              <p className="text-slate-200 font-light">{slide.description}</p>
+
               <Button
                 text={slide["button-title"]}
                 variant="secondary-white-np"
